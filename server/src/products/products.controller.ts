df084bc8f69a947f97c2d5ce226c41449c8f8679
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-15 18:28:21 -0300

    :sparkles: (server): Add product creation route

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
+
+  @Post()
+  createProduct(
+    @Body('name') name: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ): Product {
+    return this.productsService.createProduct(name, description, price);
+  }
 }
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+import { Product } from './product.model';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products: Product[] = [];
+
+  createProduct(name: string, description: string, price: number): Product {
+    const product: Product = {
+      id: uuid(),
+      slug: null,
+      name,
+      description,
+      price,
+    };
+    product.slug = this.generateSlug(product.name);
+
+    this.products.push(product);
+    return product;
+  }
+
+  private generateSlug(name: string): string {
+    return name.split(' ').join('-').toLowerCase();
+  }
+}
