9dcbea71b6e8fb391cfbe033596ed7a84d398cf6
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-17 09:08:20 -0300

    :goal_net: (server): Apply create product DTO to its service and controller

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './product.model';
+import { CreateProductDTO } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,10 +9,8 @@ export class ProductsController {
 
   @Post()
   createProduct(
-    @Body('name') name: string,
-    @Body('description') description: string,
-    @Body('price') price: number,
+    @Body(ValidationPipe) createProductDto: CreateProductDTO,
   ): Product {
-    return this.productsService.createProduct(name, description, price);
+    return this.productsService.createProduct(createProductDto);
   }
 }
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { v1 as uuid } from 'uuid';
 import { Product } from './product.model';
+import { CreateProductDTO } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductsService {
   private products: Product[] = [];
 
-  createProduct(name: string, description: string, price: number): Product {
+  createProduct(createProductDto: CreateProductDTO): Product {
+    const { name, description, price } = createProductDto;
+
     const product: Product = {
       id: uuid(),
       name,
