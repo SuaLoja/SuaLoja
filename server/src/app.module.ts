620e37a75d66bc41cd49c54a9daaf20a97e632c0
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-22 20:13:20 -0300

    :building_construction: (server): Make architectural changes
    Move products module to 'src/modules'

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(TypeOrmConfig), ProductsModule],
