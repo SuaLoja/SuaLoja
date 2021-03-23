962957d00ddce32b7b8636a5a420c0967bcbb3ef
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-23 16:45:39 -0300

    :label: (server): Add store entity

--- server/src/modules/stores/stores.module.ts
+++ server/src/modules/stores/stores.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Store } from './store.entity';
 import { StoresController } from './stores.controller';
 import { StoresService } from './stores.service';
 
 @Module({
   controllers: [StoresController],
   providers: [StoresService],
+  imports: [TypeOrmModule.forFeature([Store])],
 })
 export class StoresModule {}
