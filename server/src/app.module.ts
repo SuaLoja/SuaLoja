e38140c52bbbb57c6cc158348910a905e1cb2878
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-29 10:46:35 -0300

    :sparkles: (server): Add auth module, controller and service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
+import { AuthController } from './modules/auth/auth.controller';
+import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
 import { StoresModule } from './modules/stores/stores.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), ProductsModule, StoresModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    ProductsModule,
+    StoresModule,
+    AuthModule,
+  ],
+  controllers: [AuthController],
 })
 export class AppModule {}
