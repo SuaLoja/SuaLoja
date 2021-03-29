fc680cbc7193010a1ba030a623eeacc3d06b7703
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-29 11:33:03 -0300

    :bug: (server): Remove auth controller from app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
-import { AuthController } from './modules/auth/auth.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
 import { StoresModule } from './modules/stores/stores.module';
@@ -13,6 +12,5 @@ import { StoresModule } from './modules/stores/stores.module';
     StoresModule,
     AuthModule,
   ],
-  controllers: [AuthController],
 })
 export class AppModule {}
