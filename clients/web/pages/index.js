faba978d32c505548974465edf7fe5f335df5cd7
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-03-30 16:07:41 -0300

    :sparkles: (web): Create web project
    Generated with "create-next-app"

---
+++ clients/web/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>SuaLoja</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Hello World
+        </h1>
+      </main>
+    </div>
+  );
+}
