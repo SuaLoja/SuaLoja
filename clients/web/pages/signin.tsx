fc2ce087f4a827007c007db2e6cc4cebc4f33ae0
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-11 11:59:54 -0300

    :globe_with_meridians: (web): Translate signin page

--- clients/web/pages/signin.tsx
+++ clients/web/pages/signin.tsx
@@ -12,7 +12,7 @@ export default function SignIn() {
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
                 <h1 className="h1">
-                  Welcome back. We exist to make entrepreneurism easier.
+                  Bem vindo de volta. Faça login em sua conta.
                 </h1>
               </div>
               <div className="max-w-sm mx-auto">
@@ -29,7 +29,7 @@ export default function SignIn() {
                         id="email"
                         type="email"
                         className="form-input w-full text-gray-800"
-                        placeholder="Enter your email address"
+                        placeholder="Digite seu email"
                         required
                       />
                     </div>
@@ -41,20 +41,20 @@ export default function SignIn() {
                           className="block text-gray-800 text-sm font-medium mb-1"
                           htmlFor="password"
                         >
-                          Password
+                          Senha
                         </label>
                         <a
                           href="reset-password"
                           className="text-sm font-medium text-blue-600 hover:underline"
                         >
-                          Having trouble signing in?
+                          Tendo problemas pra entrar?
                         </a>
                       </div>
                       <input
                         id="password"
                         type="password"
                         className="form-input w-full text-gray-800"
-                        placeholder="Enter your password"
+                        placeholder="Digite sua senha"
                         required
                       />
                     </div>
@@ -65,7 +65,7 @@ export default function SignIn() {
                         <label className="flex items-center">
                           <input type="checkbox" className="form-checkbox" />
                           <span className="text-gray-600 ml-2">
-                            Keep me signed in
+                            Mantenha-me conectado
                           </span>
                         </label>
                       </div>
@@ -77,7 +77,7 @@ export default function SignIn() {
                         className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
                         type="button"
                       >
-                        Sign in
+                        Entrar
                       </button>
                     </div>
                   </div>
@@ -87,60 +87,21 @@ export default function SignIn() {
                     className="border-t border-gray-300 flex-grow mr-3"
                     aria-hidden="true"
                   />
-                  <div className="text-gray-600 italic">Or</div>
+                  <div className="text-gray-600 italic">Ou</div>
                   <div
                     className="border-t border-gray-300 flex-grow ml-3"
                     aria-hidden="true"
                   />
                 </div>
-                <form>
-                  <div className="flex flex-wrap -mx-3 mb-3">
-                    <div className="w-full px-3">
-                      <button
-                        className="btn px-0 text-white bg-gray-900 hover:bg-gray-800 w-full relative flex items-center"
-                        type="button"
-                      >
-                        <svg
-                          className="w-4 h-4 fill-current text-white opacity-75 flex-shrink-0 mx-4"
-                          viewBox="0 0 16 16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M7.95 0C3.578 0 0 3.578 0 7.95c0 3.479 2.286 6.46 5.466 7.553.397.1.497-.199.497-.397v-1.392c-2.187.497-2.683-.993-2.683-.993-.398-.895-.895-1.193-.895-1.193-.696-.497.1-.497.1-.497.795.1 1.192.795 1.192.795.696 1.292 1.888.895 2.286.696.1-.497.298-.895.497-1.093-1.79-.2-3.578-.895-3.578-3.975 0-.895.298-1.59.795-2.087-.1-.2-.397-.994.1-2.087 0 0 .695-.2 2.186.795a6.408 6.408 0 011.987-.299c.696 0 1.392.1 1.988.299 1.49-.994 2.186-.795 2.186-.795.398 1.093.199 1.888.1 2.087.496.596.795 1.291.795 2.087 0 3.08-1.889 3.677-3.677 3.875.298.398.596.895.596 1.59v2.187c0 .198.1.497.596.397C13.714 14.41 16 11.43 16 7.95 15.9 3.578 12.323 0 7.95 0z" />
-                        </svg>
-                        <span className="flex-auto pl-16 pr-8 -ml-16">
-                          Continue with GitHub
-                        </span>
-                      </button>
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap -mx-3">
-                    <div className="w-full px-3">
-                      <button
-                        className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center"
-                        type="button"
-                      >
-                        <svg
-                          className="w-4 h-4 fill-current text-white opacity-75 flex-shrink-0 mx-4"
-                          viewBox="0 0 16 16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
-                        </svg>
-                        <span className="flex-auto pl-16 pr-8 -ml-16">
-                          Continue with Google
-                        </span>
-                      </button>
-                    </div>
-                  </div>
-                </form>
                 <div className="text-gray-600 text-center mt-6">
-                  Don’t you have an account?{' '}
+                  Não possui uma conta?{' '}
                   <a
                     href="/signup"
                     className="text-blue-600 hover:underline transition duration-150 ease-in-out"
                   >
-                    Sign up
+                    Crie uma gratuitamente
                   </a>
+                  .
                 </div>
               </div>
             </div>
