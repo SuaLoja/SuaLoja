b331b32a7587421c7db08a5247fe227f51465e36
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-13 09:06:57 -0300

    :sparkles: (server): Use env variables in typeorm config

--- packages/server/src/config/typeorm.config.ts
+++ packages/server/src/config/typeorm.config.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env
+
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'sualoja',
+  host: DB_HOST || 'localhost',
+  port: Number(DB_PORT) || 5432,
+  username: DB_USER || 'postgres',
+  password: DB_PASS || 'postgres',
+  database: DB_NAME || 'sualoja',
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
   synchronize: true
 }
