c207345d8f3944d9ebd24d2cc4ec681ba8fdbc29
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-14 19:17:53 -0300

    :bug: (web): Fix bg color class in form message

--- packages/web/components/Form/Message.tsx
+++ packages/web/components/Form/Message.tsx
@@ -7,7 +7,11 @@ interface MessageProps {
 
 const Message: React.FC<MessageProps> = ({ color, text }) => {
   return (
-    <div className={`text-center bg-${color} mb-4 py-3 px-3 sm:px-6 lg:px-8`}>
+    <div
+      className={`text-center ${
+        'bg-' + color + '-500'
+      } mb-4 py-3 px-3 sm:px-6 lg:px-8 rounded`}
+    >
       <p className="font-medium text-white">{text}</p>
     </div>
   )
