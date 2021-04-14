9ebb2e512ab2fc9a837324361a51f48fff3d3519
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-14 12:11:10 -0300

    :pencil2: (web): Rename message prop in form message

--- packages/web/components/Form/Message.tsx
+++ packages/web/components/Form/Message.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 interface MessageProps {
   color: string
-  message: string
+  text: string
 }
 
-const Message: React.FC<MessageProps> = ({ color, message }) => {
+const Message: React.FC<MessageProps> = ({ color, text }) => {
   return (
     <div className={`text-center bg-${color} mb-4 py-3 px-3 sm:px-6 lg:px-8`}>
-      <p className="font-medium text-white">{message}</p>
+      <p className="font-medium text-white">{text}</p>
     </div>
   )
 }
