aa3da166e6cef299550b10eaba89fd94e4dc2583
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-24 12:05:48 -0300

    :lipstick: (dashboard): Add signup page

---
+++ packages/dashboard/app/Http/Controllers/Auth/SignupController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SignupController extends Controller
+{
+    public function index()
+    {
+        return view('auth.signup');
+    }
+}
