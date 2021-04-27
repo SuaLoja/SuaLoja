bf4e965ed746b28740d4773bd76b90f7c98f4dbb
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-26 22:52:12 -0300

    :card_file_box: (web): Add one to one relationship between Users and Stores

--- packages/web/app/Models/User.php
+++ packages/web/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -40,4 +39,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function store()
+    {
+        return $this->hasOne(Store::class);
+    }
 }
--- packages/web/database/migrations/2021_04_26_125834_create_stores_table.php
+++ packages/web/database/migrations/2021_04_26_125834_create_stores_table.php
@@ -15,6 +15,7 @@ class CreateStoresTable extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name', 12);
             $table->timestamps();
         });
