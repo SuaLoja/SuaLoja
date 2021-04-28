c2eb77ce278553938ee6eb6ca615bf01b310f344
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-04-28 17:09:51 -0300

    :card_file_box: (web): Add Product model, migration and factory

---
+++ packages/web/database/migrations/2021_04_28_162701_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 40);
+            $table->string('slug', 40)->unique();
+            $table->string('description');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
