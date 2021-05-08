c997d0c5a3a73f060e59a014bea028aa943f32a2
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-05-08 19:18:44 -0300

    :sparkles: Add category select input to products routes

--- resources/views/dashboard/products/edit.blade.php
+++ resources/views/dashboard/products/edit.blade.php
@@ -54,6 +54,23 @@
                         @enderror
                     </div>
                 </div>
+                <div class="flex flex-wrap -mx-3 mb-4">
+                    <div class="w-full px-3">
+                        <label class="block text-gray-800 text-sm font-medium mb-1">
+                            Categoria
+                        </label>
+                        <select name="category"
+                                class="form-select w-full text-gray-800 px-3 py-2 pr-12 rounded
+                                border-gray-300 focus:ring-blue-500 focus:ring-opacity-50">
+                            <option value="null"></option>
+                            @foreach($categories as $category)
+                                <option value="{{ $category["id"] }}">
+                                    {{ $category["name"] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
             <div class="flex justify-between">
                 <a href="{{ route('dashboard.products') }}"
