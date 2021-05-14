1726a8e1919021d3d18b919998a80becad6d15d9
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-05-14 09:26:27 -0300

    :pencil2: Rename "quantityInStock" to "quantity_in_stock"

--- database/migrations/2021_05_14_002740_add_quantity_in_stock_to_products_table.php
+++ database/migrations/2021_05_14_002740_add_quantity_in_stock_to_products_table.php
@@ -14,7 +14,7 @@ class AddQuantityInStockToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->integer('quantityInStock');
+            $table->integer('quantity_in_stock');
         });
     }
 
@@ -26,7 +26,7 @@ class AddQuantityInStockToProductsTable extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('quantityInStock');
+            $table->dropColumn('quantity_in_stock');
         });
     }
 }
