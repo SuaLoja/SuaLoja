d9a5db828235c50a3ef189a80b9c874697e310fd
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-05-23 16:39:31 -0300

    :card_file_box: Add product image model

--- database/migrations/2021_05_23_141155_create_product_images_table.php
+++ database/migrations/2021_05_23_141155_create_product_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddImageToProductsTable extends Migration
+class CreateProductImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class AddImageToProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('image_path');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +28,6 @@ class AddImageToProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('image_path');
-        });
+        Schema::dropIfExists('product_images');
     }
 }
