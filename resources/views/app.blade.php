15339484d988efbd59b868a9d2a8715290ce64d5
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-05-25 15:07:45 -0300

    :wrench: Configure Inertia

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuaLoja</title>
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @routes
+</head>
+
+<body class="font-inter antialiased bg-white text-gray-900 tracking-tight">
+    @inertia
+</body>
+
+</html>
