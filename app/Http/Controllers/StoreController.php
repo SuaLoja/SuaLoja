64c5c7b801171c1cc50c5bc799a2bc079aa87709
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-06-06 20:29:37 -0300

    :lipstick: Update store ui

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,35 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Store;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
 
 class StoreController extends Controller
 {
     public function index(Store $store)
     {
+        $products = null;
+
+        if (Request::query('category_id')) {
+            $products = $store->products()->where(
+                'category_id', Request::query('category_id')
+            )->paginate(9);
+        } else {
+            $products = $store->products()->orderBy('title')->paginate(9);
+        }
+
         return Response::view('store.index', [
             'store' => $store,
-            'featured_product' => Product::orderBy('quantity_in_stock')->first()
+            'categories' => $store->categories()->orderBy('name')->get(),
+            'products' => $products
         ]);
     }
 
-    public function allProducts(Store $store)
-    {
-        return Response::view('store.products', [
-            'store' => $store,
-            'products' => $store->products()->simplePaginate(9),
-        ]);
-    }
-
-    public function allCategories(Store $store)
-    {
-        return Response::view('store.categories', [
-            'store' => $store,
-            'categories' => $store->categories()->simplePaginate(9),
-        ]);
-    }
-
-    public function showProduct(Store $store, Product $product)
+    public function show(Store $store, Product $product)
     {
         return Response::view('store.product', [
             'store' => $store,
