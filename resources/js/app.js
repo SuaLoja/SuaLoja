b590b3818c7199e5023c2a36845701048e6be9a6
Victor Guedes <victorfelipe.guedes@hotmail.com>
2021-06-23 16:00:09 -0300

    :heavy_minus_sign: Remove InertiaJS dependency
    Unnecessary dependency.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { InertiaApp } from '@inertiajs/inertia-react'
-import { InertiaProgress } from '@inertiajs/progress'
-
-InertiaProgress.init()
-
-const app = document.getElementById('app')
-
-render(
-  <InertiaApp
-    initialPage={JSON.parse(app.dataset.page)}
-    resolveComponent={name => require(`./Pages/${name}`).default}
-  />,
-  app
-)
+require('./bootstrap');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 const mix = require('laravel-mix')
-const path = require('path')
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,17 +12,4 @@ const path = require('path')
 
 mix
   .js('resources/js/app.js', 'public/js')
-  .react()
-  .postCss('resources/css/app.css', 'public/css', [
-    require('tailwindcss')
-  ])
-  .webpackConfig({
-    output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
-    resolve: {
-      alias: {
-        '@': path.resolve('resources/js')
-      }
-    }
-  })
-  .version()
-  .sourceMaps()
+  .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
